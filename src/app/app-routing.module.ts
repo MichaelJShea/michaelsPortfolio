31458e41708b87e7b47e7026daea3440bca4fc3e
Michael Shea <MichaelShea1814@gmail.com>
2020-02-10 22:36:55 -0600

    new theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '',             component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),    
+    CommonModule,
+    BrowserModule,],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+// import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
+    FormsModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
